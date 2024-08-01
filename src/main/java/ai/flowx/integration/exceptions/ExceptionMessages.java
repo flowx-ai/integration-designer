f547a190c4a721ee68f4e8d5e9252df04b74efe7
calin-nicoara <calin.nicoara@flowx.ai>
2024-08-01 12:54:16 +0300

    feat: mt-234 -  create sequence for workflow node

--- src/main/java/ai/flowx/integration/exceptions/ExceptionMessages.java
+++ src/main/java/ai/flowx/integration/exceptions/ExceptionMessages.java
@@ -13,7 +13,7 @@ public class ExceptionMessages {
     public static final String PARAMETER_NOT_FOUND = "Endpoint parameter not found.";
     public static final String ID_NOT_NULL = "Id must be null.";
     public static final String RESPONSE_NOT_FOUND = "Endpoint response not found.";
-    public static final String WORKFLOW_NOT_FOUND = "Workflow not found.";
+    public static final String WORKFLOW_NOT_FOUND = "Workflow not found";
     public static final String WORKFLOW_NAME_REQUIRED = "Workflow name is required.";
     public static final String WORKFLOW_NAME_EXISTS = "A different workflow exists with this name.";
     public static final String INVALID_ID = "Invalid id";
@@ -23,6 +23,13 @@ public class ExceptionMessages {
     public static final String NODE_NOT_UPDATED = "Workflow node not updated.";
     public static final String WORKFLOW_NODE_NOT_FOUND = "Workflow node not found.";
     public static final String WORKFLOW_NOT_UPDATED = "Workflow not updated.";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID = "Workflow node sequence is not valid!";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID_ONE_SEQUENCE_ALLOWED = "Node already has sequence!";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID_END_NODE_NOT_ALLOWED = "End node not allowed to have sequence!";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID_REST_NODE_HAS_STATUS_OUTPUT = "Rest node has that status output type in sequences!";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID_FORK_CONDITION_ID_NOT_FOUND = "Fork condition id does not exist!";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID_SRC_TARGET_BOTH_START_END = "One of the nodes must be different type from START/END!";
+    public static final String WORKFLOW_NODE_SEQUENCE_NOT_VALID_START_TO_END = "START node cannot have sequence to END node!";
 
     ExceptionMessages() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
--- src/main/java/ai/flowx/integration/repository/WorkflowNodeRepository.java
+++ src/main/java/ai/flowx/integration/repository/WorkflowNodeRepository.java
@@ -4,7 +4,10 @@ import ai.flowx.integration.domain.WorkflowNode;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkflowNodeRepository extends MongoRepository<WorkflowNode, String>, CustomWorkflowNodeRepository {
 
+    List<WorkflowNode> findByIdOrFlowxUuid(String id, String flowxUuid);
 }
